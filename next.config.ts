894f0fbf1709fd14c1b5d27733bf61234562e1ec
Muhammad Syafiq - Web Developer <cenalcenul2011@gmail.com>
2026-07-19 10:51:54 +0700

    Fix formatting in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   transpilePackages: ["three"],
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
 };
-
 export default nextConfig;
